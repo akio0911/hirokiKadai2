85df8c58f90902d224a45e0d85f276143ee4f3f4
akio0911 <akio0911@gmail.com>
2021-12-19 10:34:10 +0900

    feedback

--- hirokiKadai2/ContentView.swift
+++ hirokiKadai2/ContentView.swift
@@ -1,35 +1,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Operator: Int {
+        case addition
+        case subtraction
+        case multiplication
+        case division
+    }
+
     @State private var num1: Double?
     @State private var num2: Double?
     @State private var total: String = "0"
-    @State private var operatorNumber: Int = 1
+    @State private var operatorNumber: Int = Operator.addition.rawValue
     
     var body: some View {
         VStack {
             NumberInputView(value: $num1)
             NumberInputView(value: $num2)
             Picker(selection: $operatorNumber, label: Text("Picker")) {
-                Text("+").tag(1)
-                Text("-").tag(2)
-                Text("×").tag(3)
-                Text("÷").tag(4)
+                Text("+").tag(Operator.addition.rawValue)
+                Text("-").tag(Operator.subtraction.rawValue)
+                Text("×").tag(Operator.multiplication.rawValue)
+                Text("÷").tag(Operator.division.rawValue)
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
             
             Button(action: {
-                let unwrapedNum = [num1,num2].map{$0 ?? 0}
-                
-                if operatorNumber==1 {
+                let unwrapedNum = [num1,num2].map{ $0 ?? 0 }
+
+                guard let ope = Operator(rawValue: operatorNumber) else { return }
+
+                switch ope {
+                case .addition:
                     total = String(unwrapedNum[0] + unwrapedNum[1])
-                } else if operatorNumber==2 {
+                case .subtraction:
                     total = String(unwrapedNum[0] - unwrapedNum[1])
-                } else if operatorNumber==3 {
+                case .multiplication:
                     total = String(unwrapedNum[0] * unwrapedNum[1])
-                } else {
-                    if unwrapedNum[1]==0 {
+                case .division:
+                    if unwrapedNum[1] == 0 {
                         total = "割る数には0以外を入力してください"
                     } else {
                         total = String(unwrapedNum[0] / unwrapedNum[1])
